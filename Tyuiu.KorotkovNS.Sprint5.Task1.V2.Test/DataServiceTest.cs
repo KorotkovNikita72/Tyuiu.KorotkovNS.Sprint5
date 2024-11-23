deef070c067b0c45e3d840868e0373e03e72f500
TRESH 567 <tresh78803@gmail.com>
2024-11-23 17:11:03 +0500

    Task1

---
+++ Tyuiu.KorotkovNS.Sprint5.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.KorotkovNS.Sprint5.Task1.V2.Lib;
+using System.IO;
+namespace Tyuiu.KorotkovNS.Sprint5.Task1.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Nikita\AppData\Local\Temp\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+
+    }
+}
---
+++ Tyuiu.KorotkovNS.Sprint5.Task1.V2/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.KorotkovNS.Sprint5.Task1.V2.Lib;
+namespace Tyuiu.KorotkovNS.Sprint5.Task1.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = -5;
+            int stopValue = 5;
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Коротков Н. С. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс file. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Коротков Никита Сергеевич | ИСТНб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение и результат сохранить в файл      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue,stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
