b6e978855e58f34d807a385b2ae9ba64a715bbb9
TRESH 567 <tresh78803@gmail.com>
2024-11-23 16:23:42 +0500

    Task0

--- Tyuiu.KorotkovNS.Sprint5.Task0.V15.Lib/DataService.cs
+++ Tyuiu.KorotkovNS.Sprint5.Task0.V15.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KorotkovNS.Sprint5.Task0.V15.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
             double y = x / (Math.Pow(x, 3) + 2);
             y = Math.Round(y, 3);
             File.WriteAllText(path, Convert.ToString(y));
