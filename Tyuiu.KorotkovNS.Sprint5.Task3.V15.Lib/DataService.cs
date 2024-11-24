ffcfa29605d2f11be133c74cd5ff0813758113a2
TRESH 567 <tresh78803@gmail.com>
2024-11-24 15:27:01 +0500

    Task3

---
+++ Tyuiu.KorotkovNS.Sprint5.Task3.V15.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+namespace Tyuiu.KorotkovNS.Sprint5.Task3.V15.Lib
+{
+    public class DataService : ISprint5Task3V15
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            double y = -1.4 * Math.Pow(x, 3) + 2.3 * Math.Pow(x, 2) + 0.6 * x;
+
+            y = Math.Round(y, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KorotkovNS.Sprint5.Task3.V15/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.KorotkovNS.Sprint5.Task3.V15.Lib;
+namespace Tyuiu.KorotkovNS.Sprint5.Task3.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 5;
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Коротков Н. С. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс file. Потоковый метод записи данных в бинарный файл         *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #15                                                              *");
+            Console.WriteLine("* Выполнил: Коротков Никита Сергеевич | ИСТНб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, вычислить его значение при х = 5                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
