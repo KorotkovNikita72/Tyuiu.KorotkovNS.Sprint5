778b6183ac88ea562831a74f7dd736f1583e4604
TRESH 567 <tresh78803@gmail.com>
2024-11-25 12:20:39 +0500

    Task6

---
+++ Tyuiu.KorotkovNS.Sprint5.Task6.V11.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+using Tyuiu.KorotkovNS.Sprint5.Task6.V11.Lib;
+using System.IO;
+namespace Tyuiu.KorotkovNS.Sprint5.Task6.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V11.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V11.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
